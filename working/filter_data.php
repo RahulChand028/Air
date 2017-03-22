441e54965a5a1c57399e005543bf00b67e3fdd86
Rahul Chand <chandrahul028@gmail.com>
2017-03-22 19:28:58 +0530

    Update filter_data.php

--- working/filter_data.php
+++ working/filter_data.php
@@ -1,7 +1,13 @@
-             public function validate_url($url){
+<?php
+
+namespace Air/data_filter;
+class data_filter{
+
+            public function validate_url($url){
                  if(preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$url)){
                       return 1;
                  }else{
                       return 0;
                  }
             }
+}
